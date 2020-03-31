86430219d36c245c58e287d26be8d9da38ff81fa
SwarnaMadhuriP <54323047+SwarnaMadhuriP@users.noreply.github.com>
2020-03-31 19:40:00 +0530

    8

--- MainActivity.java
+++ MainActivity.java
@@ -1,72 +1,108 @@
-package com.example.gps;
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
+package com.example.sd_card;
 
-import android.Manifest;
-import android.app.Activity;
-import android.content.pm.PackageManager;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
-//import android.support.v4.app.ActivityCompat;
-//import android.test.mock.MockPackageManager;
+import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class MainActivity extends AppCompatActivity {
-
-    Button btnShowLocation;
-    private static final int REQUEST_CODE_PERMISSION = 2;
-    String mPermission = Manifest.permission.ACCESS_FINE_LOCATION;
-
-    // GPSTracker class
-    GPSTracker gps;
+    EditText e1;
+    Button read, write, clear;
+    private String filename = "SampleFile.txt";
+    private String filepath = "MyFileStorage";
+    File myFile;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        try {
-            if (ActivityCompat.checkSelfPermission(this, mPermission)
-                    != PackageManager.PERMISSION_GRANTED) {
+        e1 = (EditText)findViewById(R.id.editText);
+        write = (Button)findViewById((R.id.button));
+        read = (Button)findViewById(R.id.button2);
+        clear = (Button)findViewById(R.id.button3);
 
-                ActivityCompat.requestPermissions(this, new String[]{mPermission},
-                        REQUEST_CODE_PERMISSION);
 
-                // If any permission above not allowed by user, this condition wil execute every time, else your else part will work
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+
+        if (!isExternalStorageAvailable() || isExternalStorageReadOnly()) {
+            write.setEnabled(false);
+            read.setEnabled(false);
+            clear.setEnabled(false);
+        }else{
+            myFile = new File(getExternalFilesDir(filepath), filename);
         }
 
-        btnShowLocation = (Button) findViewById(R.id.button);
-
-        // show location button click event
-        btnShowLocation.setOnClickListener(new View.OnClickListener() {
-
+        write.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View arg0) {
-                // create class object
-                gps = new GPSTracker(MainActivity.this);
-
-                // check if GPS enabled
-                if(gps.canGetLocation()){
-                    // if(gps.getLocation()!=null){
-                    double latitude = gps.getLatitude();
-                    double longitude = gps.getLongitude();
-
-                    // \n is for new line
-                    Toast.makeText(getApplicationContext(), "Your Location is - \nLat: "
-                            + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
-                }else{
-                    // can't get location
-                    // GPS or Network is not enabled
-                    // Ask user to enable GPS/network in settings
-                    gps.showSettingsAlert();
+            public void onClick(View v) {
+                String msg = e1.getText().toString();
+                try{
+                    FileOutputStream fo = new FileOutputStream(myFile);
+                    fo.write(msg.getBytes());
+                    fo.close();
+                    Toast.makeText(getBaseContext(), "Data wrote to SD Card", Toast.LENGTH_LONG).show();
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
+            }
+        });
 
+        read.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                String message;
+                String buf = "";
+                try {
+                    FileInputStream fin = new FileInputStream(myFile);
+                    DataInputStream in = new DataInputStream(fin);
+                    BufferedReader br = new BufferedReader(new InputStreamReader(in));
+                    while((message = br.readLine()) != null) {
+                        buf += message;
+                    }
+                    e1.setText(buf);
+                    br.close();
+                    fin.close();
+                    Toast.makeText(getBaseContext(),"Data Recived from SDCARD",Toast.LENGTH_LONG).show();
+                } catch(Exception e) {
+                    Toast.makeText(getBaseContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        clear.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                e1.setText("");
             }
         });
     }
+
+    private static boolean isExternalStorageReadOnly(){
+        String extStorageState = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED_READ_ONLY.equals(extStorageState)) {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean isExternalStorageAvailable(){
+        String extStorageState = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(extStorageState)) {
+            return true;
+        }
+        return false;
+    }
+
 }
